c2b20ef2c20efa8bad03f0469f6889402df37a0c
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2021-01-11 17:50:56 +0000

    firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMLsh5TexL1bSVesmEu6pplZihE7dh_IU",
+    authDomain: "web-dev-assessment-20500.firebaseapp.com",
+    projectId: "web-dev-assessment-20500",
+    storageBucket: "web-dev-assessment-20500.appspot.com",
+    messagingSenderId: "211960964862",
+    appId: "1:211960964862:web:9dda793b4ac0173c85a7af"
+};
+
+export default firebaseConfig;
