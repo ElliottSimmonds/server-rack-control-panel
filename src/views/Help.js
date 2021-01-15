12ce964f77d73c84792f3b9f29b1c6bbc8c102f3
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2021-01-15 10:21:28 +0000

    Added page details to help page

--- src/views/Help.js
+++ src/views/Help.js
@@ -18,24 +18,34 @@ function Help() {
     return (
         <ViewWrapper>
             <Grid container justify="center">
-                <Grid item xs={10} sm={8} md={6} lg={4}>
+                <Grid item xs={12} sm={10} md={8} lg={6}>
                     <Tile>
-                        <h2>Page Guide</h2>
+                        <h2>Page Information</h2>
                         <h4>Dashboard</h4>
                         <StyledP>
-                            test
+                        Contains a list of servers on the network
+
+                        <ul>
+                            <li>Light indicators represent the power state of each system. Green for on, red for off and yellow for restarting.</li>
+                            <li>Click systems to expand and display additional information and controls.</li>
+                            <li>Create new system entries with the "Add system" button.</li>
+                            <li>After entering the required details, the rest is automatically generated to simulate possible real world system information.</li>
+                            <li>The details of existing systems can be edited by clicking the "edit" button within their expanded view.</li>
+                        </ul>  
+
                         </StyledP>
                         <h4>User Settings</h4>
                         <StyledP>
-
+                        Change account details such as email and password. Account can also be deleted from here.
                         </StyledP>
                         <h4>User Account Control</h4>
                         <StyledP>
-
+                        From this page, admin users can create new accounts and change the permissions of existing users.
                         </StyledP>
                         <h4>Settings</h4>
                         <StyledP>
-
+                        Page to control the client side application features, including the disabling of notifications and toggling dark mode.
+                        Currently not implemented.
                         </StyledP>
                     </Tile>
                 </Grid>
