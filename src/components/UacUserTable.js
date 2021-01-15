d7ea77ed96d346f27d2aa163734e95ec661057a2
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2021-01-15 10:28:06 +0000

    Removed unused imports

--- src/components/UacUserTable.js
+++ src/components/UacUserTable.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import moment from "moment";
 
 import Table from '@material-ui/core/Table';
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
 import Grid from '@material-ui/core/Grid';
 import Tile from '../components/Tile';
