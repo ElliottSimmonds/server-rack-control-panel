a140c8aebda1534f5f9ce40b98919252df0ed9ab
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2021-01-15 09:33:57 +0000

    Change tile shadow

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,19 +1,10 @@
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Tile = styled.div`
-    box-shadow: 40px 10px 20px rgba(31, 32, 32, ${props => props.elevation});
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.15);
     padding: 20px;
     margin: 10px;
     background-color: white;
 `;
-
-Tile.propTypes = {
-  elevation: PropTypes.string
-};
-
-Tile.defaultProps = {
-  elevation: "0.05"
-};
 
 export default Tile;
