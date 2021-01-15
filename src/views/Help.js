ad12f02247e849ec0e851640e8fe4895e8cb0670
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2021-01-15 09:35:33 +0000

    Added content to help view

--- src/views/Help.js
+++ src/views/Help.js
@@ -1,12 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
+import styled from "styled-components";
+import Tile from "../components/Tile";
+import Grid from '@material-ui/core/Grid';
+
+const ViewWrapper = styled.div`
+    @media (min-width: 960px) {
+        margin-left: 250px;
+    }
+`;
+
+const StyledP = styled.p`
+    margin-left: 20px;
+`;
 
 function Help() {
 
     return (
-        <div>
-            Help
-        </div>
+        <ViewWrapper>
+            <Grid container justify="center">
+                <Grid item xs={10} sm={8} md={6} lg={4}>
+                    <Tile>
+                        <h2>Page Guide</h2>
+                        <h4>Dashboard</h4>
+                        <StyledP>
+                            test
+                        </StyledP>
+                        <h4>User Settings</h4>
+                        <StyledP>
+
+                        </StyledP>
+                        <h4>User Account Control</h4>
+                        <StyledP>
+
+                        </StyledP>
+                        <h4>Settings</h4>
+                        <StyledP>
+
+                        </StyledP>
+                    </Tile>
+                </Grid>
+            </Grid>
+        </ViewWrapper>
     );
 }
 
